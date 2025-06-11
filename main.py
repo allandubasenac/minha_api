277f738045de38f22f419b794b34acf954407c48
henda-spec <allan.dubas@alunos.sc.br>
2025-06-11 20:53:52 -0300

    11/06

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
 from database import engine, Base
-from app.routers import empresa
-from app.models.empresa import Empresa
+from app.routers import empresa as e
+from app.models import user, empresa, borrow, book
+
+app = FastAPI()
 
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
 
 @app.get("/")
 def check_api():
     return {"response": "Api Online!"}
 
-app.include_router(empresa.router)
+app.include_router(e.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)
